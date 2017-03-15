2c534bbb6f538d8f78a8b4506006e40d9a8e2c5c
PaulDCross <paulcross@f2s.com>
2017-03-15 22:15:29 +0000

    added more files

---
+++ IROS_graphData/MADgraphs.m
@@ -0,0 +1,31 @@
+close all
+clear all
+load('TranslationZippedMADXYDepthName.mat')
+
+fig1 = figure;
+% Create axes
+axes1 = axes('Parent',fig1,'YMinorTick','on','XMinorTick','on',...
+    'XTickLabel',{'2','4','6','8','10','8','6','4','2'},...
+    'XTick',[2 4 6 8 10 12 14 16 18]);
+hold(axes1,'on');
+grid on;
+% Create xlabel
+xlabel('Displacement from start position (mm)');
+% Create ylabel
+ylabel('Average Mean Absolute Deviation (Pixels)');
+scatter(MaddeningX(:,2)/10,MaddeningX(:,1), 'xb')
+save2pdf('MeanAbsoluteDeviationofStatePinsX')
+
+fig2 = figure;
+% Create axes
+axes1 = axes('Parent',fig2,'YMinorTick','on','XMinorTick','on',...
+    'XTickLabel',{'2','4','6','8','10','8','6','4','2'},...
+    'XTick',[2 4 6 8 10 12 14 16 18]);
+hold(axes1,'on');
+grid on;
+% Create xlabel
+xlabel('Displacement from start position (mm)');
+% Create ylabel
+ylabel('Average Mean Absolute Deviation (Pixels)');
+scatter(MaddeningY(:,2)/10,MaddeningY(:,1), 'xb')
+save2pdf('MeanAbsoluteDeviationofStatePinsY')
