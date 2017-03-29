84d8c9784f0cc046e4da980a3b92e0e5b012fc85
PaulDCross <paul2.cross@live.uwe.ac.uk>
2017-03-29 21:26:09 +0100

    Plots needed changing

--- IndividualProject/ConvolutionAnalysis.m
+++ IndividualProject/ConvolutionAnalysis.m
@@ -12,6 +12,11 @@ RyNY = zeros(100,10);
 RyPX = zeros(100,10);
 RyPY = zeros(100,10);
 
+%% Save figures
+path = 'C:\Users\Paul Cross\Documents\Work\UWE Work\Bristol Robotics Lab\TSP_ProgramsData\MotionCapturePillow\IndividualProject';
+rez=200; %resolution (dpi) of final graphic
+resolution=get(0,'ScreenPixelsPerInch'); %dont need to change anything here
+
 %% Get data
 for k = 1:numfiles
   myfilename = sprintf('Convolution/RxN0%d.txt', k);
@@ -71,7 +76,7 @@ RyNYvar = var(RyNY,0,2);
 RyPXvar = var(RyPX,0,2);
 RyPYvar = var(RyPY,0,2);
 
-%% Plot data
+%% Plot ConvolutionTrackingPlot
 fig1 = figure(1);
 set(fig1,'Position',[0, 0, 1200, 1000]);
 clf
@@ -88,12 +93,17 @@ RyN = plot(RyNXmean,RyNYmean,'b.-','DisplayName','Negative rotation around the Y
 RyP = plot(RyPXmean,RyPYmean,'k.-','DisplayName','Positive rotation around the Y axis');
 legend1 = legend(axes1,'show');
 set(legend1,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig1,'ConvolutionTrackingPlot','epsc');
-saveas(fig1,'ConvolutionTrackingPlot');
 
-%% Variance
+%% Save ConvolutionTrackingPlot
+f=fig1; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'ConvolutionTrackingPlot'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'ConvolutionTrackingPlot'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot ConvolutionVarianceRxNegative
 fig2 = figure(2);
-set(fig2,'Position',[0, 50, 1200, 500]);
+set(fig2,'Position',[0, 50, 1200, 300]);
 clf
 axes2 = axes('Parent',fig2,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -107,11 +117,17 @@ RxNYvarPlot = plot(RxNYvar,'b','DisplayName','Variance of Y Coordinate');
 legend([RxNXvarPlot RxNYvarPlot],'Variance of X Coordinate','Variance of Y Coordinate','Location','NorthWest','FontSize',16,'Interpreter','latex')
 legend2 = legend(axes2,'show');
 set(legend2,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig1,'ConvolutionVarianceRxNegative','epsc');
-saveas(fig1,'ConvolutionVarianceRxNegative');
 
+%% Save ConvolutionVarianceRxNegative
+f=fig2; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'ConvolutionVarianceRxNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'ConvolutionVarianceRxNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot ConvolutionVarianceRxPositive
 fig3 = figure(3);
-set(fig3,'Position',[0, 50, 1200, 500]);
+set(fig3,'Position',[0, 50, 1200, 300]);
 clf
 axes3 = axes('Parent',fig3,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -124,11 +140,17 @@ RxPXvarPlot = plot(RxPXvar,'r','DisplayName','Variance of X Coordinate');
 RxPYvarPlot = plot(RxPYvar,'b','DisplayName','Variance of Y Coordinate');
 legend3 = legend(axes3,'show');
 set(legend3,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig1,'ConvolutionVarianceRxPositive','epsc');
-saveas(fig1,'ConvolutionVarianceRxPositive');
 
+%% Save ConvolutionVarianceRxPositive
+f=fig3; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'ConvolutionVarianceRxPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'ConvolutionVarianceRxPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot ConvolutionVarianceRyNegative
 fig4 = figure(4);
-set(fig4,'Position',[0, 50, 1200, 500]);
+set(fig4,'Position',[0, 50, 1200, 300]);
 clf
 axes4 = axes('Parent',fig4,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -141,11 +163,17 @@ RyNXvarPlot = plot(RyNXvar,'r','DisplayName','Variance of X Coordinate');
 RyNYvarPlot = plot(RyNYvar,'b','DisplayName','Variance of Y Coordinate');
 legend4 = legend(axes4,'show');
 set(legend4,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig1,'ConvolutionVarianceRyNegative','epsc');
-saveas(fig1,'ConvolutionVarianceRyNegative');
 
+%% Save ConvolutionVarianceRyNegative
+f=fig4; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'ConvolutionVarianceRyNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'ConvolutionVarianceRyNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot ConvolutionVarianceRyPositive
 fig5 = figure(5);
-set(fig5,'Position',[0, 50, 1200, 500]);
+set(fig5,'Position',[0, 50, 1200, 300]);
 clf
 axes5 = axes('Parent',fig5,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -158,8 +186,14 @@ RyPXvarPlot = plot(RyPXvar,'r','DisplayName','Variance of X Coordinate');
 RyPYvarPlot = plot(RyPYvar,'b','DisplayName','Variance of Y Coordinate');
 legend5 = legend(axes5,'show');
 set(legend5,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig1,'ConvolutionVarianceRyPositive','epsc');
-saveas(fig1,'ConvolutionVarianceRyPositive');
+
+%% Save ConvolutionVarianceRyPositive
+f=fig5; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'ConvolutionVarianceRyPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'ConvolutionVarianceRyPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
 
 
 % count = 0;
--- IndividualProject/IntersectionAnalysis.m
+++ IndividualProject/IntersectionAnalysis.m
@@ -20,6 +20,11 @@ RyPY = zeros(100,10);
 RyPXstd = zeros(100,10);
 RyPYstd = zeros(100,10);
 
+%% Save figures
+path = 'C:\Users\Paul Cross\Documents\Work\UWE Work\Bristol Robotics Lab\TSP_ProgramsData\MotionCapturePillow\IndividualProject';
+rez=200; %resolution (dpi) of final graphic
+resolution=get(0,'ScreenPixelsPerInch'); %dont need to change anything here
+
 %% Get data
 for k = 1:numfiles
   myfilename = sprintf('Intersection/RxN0%d_resolution180_100.txt', k);
@@ -99,9 +104,9 @@ RyNYstdmean = mean(RyNYstd,2);
 RyPXstdmean = mean(RyPXstd,2);
 RyPYstdmean = mean(RyPYstd,2);
 
-%% Plot data
+%% Plot IntersectionTrackingPlot
 fig1 = figure(1);
-set(fig1, 'Position', [0, 0, 1200, 1000]);
+set(fig1,'Position',[0, 0, 1200, 1000]);
 clf
 axes1 = axes('Parent',fig1,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -116,12 +121,17 @@ RyN = plot(RyNXmean,RyNYmean,'b.-','DisplayName','Negative rotation around the Y
 RyP = plot(RyPXmean,RyPYmean,'k.-','DisplayName','Positive rotation around the Y axis');
 legend1 = legend(axes1,'show');
 set(legend1,'Location','northwest','Interpreter','latex','FontSize',14);
-saveas(fig1,'IntersectionTrackingPlot','epsc');
-saveas(fig1,'IntersectionTrackingPlot');
 
-%% Variance
+%% Save IntersectionTrackingPlot
+f=fig1; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionTrackingPlot'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionTrackingPlot'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionVarianceRxNegative
 fig2 = figure(2);
-set(fig2, 'Position', [0, 50, 1200, 500]);
+set(fig2,'Position',[0, 50, 1200, 300]);
 clf
 axes2 = axes('Parent',fig2,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -135,11 +145,17 @@ RxNYvarPlot = plot(RxNYvar,'b','DisplayName','Variance of Y Coordinate');
 legend([RxNXvarPlot RxNYvarPlot], 'Variance of X Coordinate', 'Variance of Y Coordinate', 'Location', 'NorthWest', 'FontSize',16,'Interpreter','latex')
 legend2 = legend(axes2,'show');
 set(legend2,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig2,'IntersectionVarianceRxNegative','epsc');
-saveas(fig2,'IntersectionVarianceRxNegative');
 
+%% Save IntersectionVarianceRxNegative
+f=fig2; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionVarianceRxNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionVarianceRxNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionVarianceRxPositive
 fig3 = figure(3);
-set(fig3, 'Position', [0, 50, 1200, 500]);
+set(fig3,'Position',[0, 50, 1200, 300]);
 clf
 axes3 = axes('Parent',fig3,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -152,11 +168,17 @@ RxPXvarPlot = plot(RxPXvar,'r','DisplayName','Variance of X Coordinate');
 RxPYvarPlot = plot(RxPYvar,'b','DisplayName','Variance of Y Coordinate');
 legend3 = legend(axes3,'show');
 set(legend3,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig2,'IntersectionVarianceRxPositive','epsc');
-saveas(fig2,'IntersectionVarianceRxPositive');
 
+%% Save IntersectionVarianceRxPositive
+f=fig3; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionVarianceRxPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionVarianceRxPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionVarianceRyNegative
 fig4 = figure(4);
-set(fig4,'Position',[0, 50, 1200, 500]);
+set(fig4,'Position',[0, 50, 1200, 300]);
 clf
 axes4 = axes('Parent',fig4,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -169,11 +191,17 @@ RyNXvarPlot = plot(RyNXvar,'r','DisplayName','Variance of X Coordinate');
 RyNYvarPlot = plot(RyNYvar,'b','DisplayName','Variance of Y Coordinate');
 legend4 = legend(axes4,'show');
 set(legend4,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig2,'IntersectionVarianceRyNegative','epsc');
-saveas(fig2,'IntersectionVarianceRyNegative');
 
+%% Save IntersectionVarianceRyNegative
+f=fig4; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionVarianceRyNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionVarianceRyNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionVarianceRyPositive
 fig5 = figure(5);
-set(fig5,'Position',[0, 50, 1200, 500]);
+set(fig5,'Position',[0, 50, 1200, 300]);
 clf
 axes5 = axes('Parent',fig5,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -186,12 +214,23 @@ RyPXvarPlot = plot(RyPXvar,'r','DisplayName','Variance of X Coordinate');
 RyPYvarPlot = plot(RyPYvar,'b','DisplayName','Variance of Y Coordinate');
 legend5 = legend(axes5,'show');
 set(legend5,'Location','northwest','Interpreter','latex','FontSize',16);
-saveas(fig2,'IntersectionVarianceRyPositive','epsc');
-saveas(fig2,'IntersectionVarianceRyPositive');
+
+%% Save IntersectionVarianceRyPositive
+f=fig5; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionVarianceRyPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionVarianceRyPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+
+
+
+
 
 %% Standard Deviation
+%% Plot IntersectionStandardDeviationXNegative
 fig6 = figure(6);
-set(fig6,'Position',[0, 50, 1200, 500]);
+set(fig6,'Position',[0, 50, 1200, 300]);
 clf
 axes6 = axes('Parent',fig6,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -204,11 +243,17 @@ RxNXstdmeanPlot = plot(RxNXstdmean,'r','DisplayName','Standard deviation of X Co
 RxNYstdmeanPlot = plot(RxNYstdmean,'b','DisplayName','Standard deviation of Y Coordinate');
 legend6 = legend(axes6,'show');
 set(legend6,'Location','northwest','Interpreter','latex');
-saveas(fig2,'IntersectionStandardDeviationXNegative','epsc');
-saveas(fig2,'IntersectionStandardDeviationXNegative');
 
+%% Save IntersectionStandardDeviationXNegative
+f=fig6; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionStandardDeviationXNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionStandardDeviationXNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionStandardDeviationXPositive
 fig7 = figure(7);
-set(fig7,'Position',[0, 50, 1200, 500]);
+set(fig7,'Position',[0, 50, 1200, 300]);
 clf
 axes7 = axes('Parent',fig7,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -221,11 +266,17 @@ RxPXstdmeanPlot = plot(RxPXstdmean,'r','DisplayName','Standard deviation of X Co
 RxPYstdmeanPlot = plot(RxPYstdmean,'b','DisplayName','Standard deviation of Y Coordinate');
 legend7 = legend(axes7,'show');
 set(legend7,'Location','northwest','Interpreter','latex');
-saveas(fig2,'IntersectionStandardDeviationXPositive','epsc');
-saveas(fig2,'IntersectionStandardDeviationXPositive');
 
+%% Save IntersectionStandardDeviationXPositive
+f=fig7; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionStandardDeviationXPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionStandardDeviationXPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionStandardDeviationYNegative
 fig8 = figure(8);
-set(fig8,'Position',[0, 50, 1200, 500]);
+set(fig8,'Position',[0, 50, 1200, 300]);
 clf
 axes8 = axes('Parent',fig8,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -238,11 +289,17 @@ RyNXstdmeanPlot = plot(RyNXstdmean,'r','DisplayName','Standard deviation of X Co
 RyNYstdmeanPlot = plot(RyNYstdmean,'b','DisplayName','Standard deviation of Y Coordinate');
 legend8 = legend(axes8,'show');
 set(legend8,'Location','northwest','Interpreter','latex');
-saveas(fig2,'IntersectionStandardDeviationYNegative','epsc');
-saveas(fig2,'IntersectionStandardDeviationYNegative');
 
+%% Save IntersectionStandardDeviationYNegative
+f=fig8; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionStandardDeviationYNegative'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionStandardDeviationYNegative'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+%% Plot IntersectionStandardDeviationYPositive
 fig9 = figure(9);
-set(fig9,'Position',[0, 50, 1200, 500]);
+set(fig9,'Position',[0, 50, 1200, 300]);
 clf
 axes9 = axes('Parent',fig9,'XMinorTick','on','YMinorTick','on','TickLabelInterpreter','latex','fontSize',16);
 grid on;
@@ -255,8 +312,18 @@ RyPXstdmeanPlot = plot(RyPXstdmean,'r','DisplayName','Standard deviation of X Co
 RyPYstdmeanPlot = plot(RyPYstdmean,'b','DisplayName','Standard deviation of Y Coordinate');
 legend9 = legend(axes9,'show');
 set(legend9,'Location','northwest','Interpreter','latex');
-saveas(fig2,'IntersectionStandardDeviationYPositive','epsc');
-saveas(fig2,'IntersectionStandardDeviationYPositive');
+
+%% Save IntersectionStandardDeviationYPositive
+f=fig9; %f is the handle of the figure you want to export
+figpos=getpixelposition(f); %dont need to change anything here
+set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); %dont need to change anything here
+print(f,fullfile(path,'IntersectionStandardDeviationYPositive'),'-depsc',['-r',num2str(rez)],'-opengl') %save file 
+print(f,fullfile(path,'IntersectionStandardDeviationYPositive'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+
+
+
+
+
 
 % figure(3)
 % clf
